d1038a1886fb5e51376ecc7df702ef13e2858702
Joshua Prodahl <proda002@umn.edu>
2013-09-13 11:49:30 -0500

    Adding period selection

--- angular-clockpicker.js
+++ angular-clockpicker.js
@@ -1,18 +1,35 @@
 angular.module("ui.clockpicker", [])
-  .directive("clockpicker", function() {
+  .controller("ClockPickerTestController", function($scope) {
+    $scope.time = {
+      hour: 1,
+      minute: 30,
+      period: "am"
+    };
+   })
+  .directive("uiClockpicker", function() {
     return {
+      restrict: "EA",
       replace: true,
       templateUrl: "template/clockpicker.html",
+      scope: {
+        hour: "=",
+        minute: "=",
+        period: "="
+      },
       controller: function($scope, $element) {
         $scope.hourOptions = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
         $scope.minuteOptions = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
-        
+        $scope.periodOptions = ['am', 'pm'];
         $scope.selectionMode = true;
-        $scope.hour = $scope.hourOptions[0];
-        $scope.minute = $scope.minuteOptions[0];
-        
+
         $scope.selectValue = function(value) {
           $scope.selectionMode ? $scope.hour = value : $scope.minute = value;
+        };
+        $scope.selectPeriod = function(value) {
+          $scope.period = value;
+        };
+        $scope.togglePeriod = function() {
+          $scope.selectPeriod($scope.period == "am" ? "pm" : "am");
         };
         $scope.$watch("selectionMode", function(value) {
           $scope.options = value ? $scope.hourOptions : $scope.minuteOptions;
@@ -26,15 +43,20 @@ angular.module("ui.clockpicker", [])
       "<div class='ui-clockpicker'>\n" +
       "  <div class='ui-clockpicker-selection'>\n" +
       "    <a ng-click='selectionMode = true' ng-class='{selected: selectionMode}'>{{hour}}</a>:" +
-      "<a ng-click='selectionMode = false' ng-class='{selected: !selectionMode}'>{{minute}}</a> pm\n" +
+      "<a ng-click='selectionMode = false' ng-class='{selected: !selectionMode}'>{{minute}}</a> " +
+      "<a ng-click='togglePeriod()'>{{period}}</a>\n" +
       "  </div>\n" +
       "  <div class='ui-clockpicker-selector' ng-class='{minute: !selectionMode}'>\n" +
-      "     <ol>" +
-      "       <li " +
-      "         ng-repeat='option in options' " +
+      "     <ol class='ui-clockpicker-time'>\n" +
+      "       <li ng-repeat='option in options' " +
       "         ng-class='{selected: selectionMode ? hour == option : minute == option }' " +
-      "         ng-click='selectValue(option)'>{{option}}</li>" +
-      "     </ol>" +
+      "         ng-click='selectValue(option)'>{{option}}</li>\n" +
+      "     </ol>\n" +
+      "     <ol class='ui-clockpicker-period'>\n" +
+      "       <li ng-repeat='periodOption in periodOptions' " +
+      "         ng-class='{selected: period == periodOption }' " +
+      "         ng-click='selectPeriod(periodOption)'>{{periodOption}}</li>\n" +
+      "     </ol>\n" +
       "  </div>\n" +
       "</div>\n" +
       "");
